97771b81eb01ee7c61538e8baabb4fd0ba2f4158
[Mika Panday] <[pandaymika@gmail.com]>
2021-05-06 21:07:40 -0400

    I think I'm done

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const timeLeft = document.querySelector("#time-left");
 const score = document.querySelector("#score");
 
 let result = 0;
-let hitPosition 
+let hitPosition;
+let currentTime = 10;
+let timerId = null;
 
 function randomSquare() {
   squares.forEach((square) => {
@@ -14,20 +16,34 @@ function randomSquare() {
   let randomSquare = squares[Math.floor(Math.random() * 9)];
   randomSquare.classList.add("mole");
 
-  hitPosition = randomSquare.id 
+  hitPosition = randomSquare.id;
 }
 
-squares.forEach(square => {
-    square.addEventListener('mousedown' () => { if (square.id == hitPosition) {
-        result ++
-        score.textContent = result
-        hitPosition = null 
+squares.forEach((square) => {
+  square.addEventListener("mousedown", () => {
+    if (square.id == hitPosition) {
+      result++;
+      score.textContent = result;
+      hitPosition = null;
     }
-    }
-})
+  });
+});
+
 function moveMole() {
-  let timerId = null;
-  timerId = setInterval(randomSquare, 500);
+  timerId = setInterval(randomSquare, 1000);
 }
 
 moveMole();
+
+function countDown() {
+  currentTime--;
+  timeLeft.textContent = currentTime;
+
+  if (currentTime == 0) {
+    clearInterval(countDownTimerId);
+    clearInterval(timerId);
+    alert("GAME OVER! Your final score is " + result);
+  }
+}
+
+let countDownTimerId = setInterval(countDown, 1000);
