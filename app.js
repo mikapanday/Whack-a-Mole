594a65242e39bbcfdc5600bb27cd0180191a8e33
[Mika Panday] <[pandaymika@gmail.com]>
2021-05-06 20:54:42 -0400

    updated javascript 2

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
 const squares = document.querySelectorAll(".square");
-const mole = document.querySelector(",mole");
+const mole = document.querySelector(".mole");
 const timeLeft = document.querySelector("#time-left");
 const score = document.querySelector("#score");
 
 let result = 0;
+let hitPosition 
 
 function randomSquare() {
   squares.forEach((square) => {
     square.classList.remove("mole");
   });
 
-  let randomPosition = squares[Math.floor(Math.random() * 9)];
+  let randomSquare = squares[Math.floor(Math.random() * 9)];
+  randomSquare.classList.add("mole");
+
+  hitPosition = randomSquare.id 
 }
 
-randomSquare();
+squares.forEach(square => {
+    square.addEventListener('mousedown' () => { if (square.id == hitPosition) {
+        result ++
+        score.textContent = result
+        hitPosition = null 
+    }
+    }
+})
+function moveMole() {
+  let timerId = null;
+  timerId = setInterval(randomSquare, 500);
+}
+
+moveMole();
